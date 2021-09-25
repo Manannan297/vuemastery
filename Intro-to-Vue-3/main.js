0294e9c04bd9c583ec1e8eee8e2e740889ecda0e
Evgenia Isaeva <evgenia.isaeva297@gmail.com>
2021-09-25 19:03:44 +0300

    3. Attribute Binding - hometask

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
       product: 'Socks',
       description: 'A warm fuzzy pair of socks.',
       image: './assets/images/socks_green.jpg',
+      url: 'https://www.vuemastery.com/',
     }
   }
 })
