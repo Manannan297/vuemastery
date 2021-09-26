b97d7c2c69a2c198bb48313c3b414d4495e80af7
Evgenia Isaeva <evgenia.isaeva297@gmail.com>
2021-09-26 18:26:16 +0300

    8. Computed Properties

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -3,16 +3,15 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: 'Socks',
+      brand: 'Vue Mastery',
       description: 'A warm fuzzy pair of socks.',
-      image: './assets/images/socks_green.jpg',
       url: 'https://www.vuemastery.com/',
-      inStock: false,
-      inventory: 0,
       onSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
+      selectedVariant: 0,
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
       ],
       sizes: ['S', 'M', 'L', 'XL'],
     }
@@ -26,9 +25,20 @@ const app = Vue.createApp({
         this.cart -= 1
       }
     },
-    updateImage(variantImage) {
-      this.image = variantImage
+    updateVariant(index) {
+      this.selectedVariant = index
     },
-  }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
+    },
+  },
 })
 
